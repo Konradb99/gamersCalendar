6739bf6a0cffcfeb45102ff6acc583c31bd5ea75
Konradb99 <56265142+Konradb99@users.noreply.github.com>
2022-05-16 10:53:18 +0200

    Added register and login pages

---
+++ app/src/main/java/com/konradb/gameCalendar/RegisterActivity.kt
@@ -0,0 +1,33 @@
+package com.konradb.gameCalendar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.konradb.gameCalendar.MainActivity
+import com.konradb.gameCalendar.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    private val fbAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isCurrentUser()
+    }
+
+    private fun isCurrentUser(){
+        fbAuth.currentUser?.let {
+                auth ->
+            val intent : Intent = Intent(applicationContext, MainActivity::class.java).apply{
+                flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+        }
+    }
+}
